54275a3ba07adec02ca708f641f0254cc8d98311
Dominique-Rubilar <62884502+Dominique-Rubilar@users.noreply.github.com>
2020-04-08 10:52:35 -0400

    Javascript primeros pasitos

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-let texto1 = document.getElementById("mensaje1").value;
-console.log(texto1);
 
 function codificar() {
-    console.log(texto1);
+     var guardarTexto = document.getElementById("mensaje1").value;
+     console.log(guardarTexto)
+
 }
