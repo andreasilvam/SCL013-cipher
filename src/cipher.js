b6ced33dff585a994e98d3af0a0556117da65406
Dominique-Rubilar <62884502+Dominique-Rubilar@users.noreply.github.com>
2020-04-13 18:25:31 -0400

    cipher Lunes 13-04

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,23 @@
 const cipher = {
-  // ...
-};
+    encode :(offset, text) => {
+        //obtener el mesaje
+        let textoCifrado = "";
+        let resultado = "";
+        //bucle desplazamiento de la letra
+        for (let i=0; i<text.length; i++){
+            //se agrega el charCodeAt para convertir en ascii las letras
+            let ascii= text.charCodeAt(i)
+            //se agrega valor numero de desplazamiento
+            ascii = ascii + offset;
+            //solo si el ascii es menor o igual que 65 y despues si es menor o igual a 90
+            if (text.charCodeAt(i) >= 65 && text.charCodeAt(i) <= 90)
+            //formula para cambiar las letras
+            {resultado+=String.fromCharCode((ascii+offset)+26)}
 
-export default cipher;
+            return resultado;
+            
+            console.log(resultado)
+           
+        }
+    }}
+    export default cipher;
