a70668495582fb26fed969d388dfd44c43214387
Dominique-Rubilar <62884502+Dominique-Rubilar@users.noreply.github.com>
2020-04-14 23:50:10 -0400

    cipher

--- src/cipher.js
+++ src/cipher.js
@@ -2,27 +2,28 @@ const cipher = {
     encode :(offset, string) => {
         console.log(offset); 
         console.log(string);
-        //obtener el mesaje
+        //obtener el mesaje, queda vacia porq ira los nuevos valores
         let textoCifrado = "";
-        //bucle desplazamiento de la letra
+        //bucle desplazamiento de la letra 
         for (let i=0; i<string.length; i++){
             console.log("paso1");
             //se agrega el charCodeAt para convertir en ascii las letras
-            let ascii= string.charCodeAt(i);
-            // formula para las letras mayusculas del ascii (en index ya converti en mayuscula el texto delmensaje 1)
-            if ((ascii >= 65) && (ascii <= 122)){
-                textoCifrado = string.fromCharCode((ascii - 65 + offset)% 26+ 65);
-            }
-                
-                //formula
-                textoCifrado=string.fromCharCode((ascii+offset)+26);
-                
-                return textoCifrado;
-            }
+            let text = string.charCodeAt(i);
+            //formula del cifrado
+            let string = ((((text - 65) + offset) % 26) + 65);
+            console.log("paso2");
+            // ahora de cifrado a letra
+            textoCifrado = textoCifrado.fromCharCode(string);
+            // resultado para cada indice se va a guardar en la variable textoCifrado
+            textoCifrado = textoCifrado + string;
+            
+            
+            return textoCifrado;
         }
-        console.log(textoCifrado);
-        
-        
     }
-};
+    //console.log(textoCifrado);
+    
+    
+}
+
 export default cipher;
