4f5497857c095c54af8f07123c23576167ba758e
Dominique-Rubilar <62884502+Dominique-Rubilar@users.noreply.github.com>
2020-04-14 20:06:16 -0400

    actualización de cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,23 +1,28 @@
 const cipher = {
-    encode :(offset, text) => {
+    encode :(offset, string) => {
+        console.log(offset); 
+        console.log(string);
         //obtener el mesaje
         let textoCifrado = "";
-        let resultado = "";
         //bucle desplazamiento de la letra
-        for (let i=0; i<text.length; i++){
+        for (let i=0; i<string.length; i++){
+            console.log("paso1");
             //se agrega el charCodeAt para convertir en ascii las letras
-            let ascii= text.charCodeAt(i)
-            //se agrega valor numero de desplazamiento
-            ascii = ascii + offset;
-            //solo si el ascii es menor o igual que 65 y despues si es menor o igual a 90
-            if (text.charCodeAt(i) >= 65 && text.charCodeAt(i) <= 90)
-            //formula para cambiar las letras
-            {resultado+=String.fromCharCode((ascii+offset)+26)}
-
-            return resultado;
-            
-            console.log(resultado)
-           
+            let ascii= string.charCodeAt(i);
+            // formula para las letras mayusculas del ascii (en index ya converti en mayuscula el texto delmensaje 1)
+            if ((ascii >= 65) && (ascii <= 122)){
+                textoCifrado = string.fromCharCode((ascii - 65 + offset)% 26+ 65);
+            }
+                
+                //formula
+                textoCifrado=string.fromCharCode((ascii+offset)+26);
+                
+                return textoCifrado;
+            }
         }
-    }}
-    export default cipher;
+        console.log(textoCifrado);
+        
+        
+    }
+};
+export default cipher;
