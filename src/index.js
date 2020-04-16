aaad6bd361f0adf776766ae87c561db621fc8a34
Dominique-Rubilar <62884502+Dominique-Rubilar@users.noreply.github.com>
2020-04-15 21:51:27 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
 import cipher from './cipher.js';
-let btnCodificar = document.getElementById('codificar');
 
-  btnCodificar.addEventListener("click", function () {
-  //guardar el mensaje con mayusculas
-  let string = document.getElementById('mensaje1').value.toUpperCase();
-//guardar el desplazamiento
-  let offset = document.getElementById('number').value;
-
-  let salidaTexto = document.getElementById("salidaTexto");
-
-  //variable para guardar el vslor del input con desplazamiento 
-  let resultado = cipher.encode (offset, string);
-  salidaTexto.innerHTML= resultado;
-  
-  console.log(resultado);
-  });
-  
+const btnCod =document.getElementById("codificar");
+btnCod.addEventListener("click",() => {
+  let string = document.getElementById("mensaje1").value.toUpperCase();
+  console.log (string);
+  let offset =  parseInt(document.getElementById("numb").value);
+  console.log (offset);
+  //let resultado = document.getElementById("salidaTexto");
+  document.getElementById("mensaje-codificado").innerHTML = window.cipher.encode (string,offset);
+});
